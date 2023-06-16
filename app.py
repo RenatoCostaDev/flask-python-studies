f7c7dff5f894b53529ac1cda2a20ea9dd7f000cb
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-16 12:22:07 -0300

    feat: render -> list and dict

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 frutas = ['Morango', 'Uva', 'Maçã', 'Laranja']
-
+notas = {'João': 5.0, 'Ana': 6.0, 'Luciana': 10}
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,13 +12,14 @@ def principal():
         'index.html',
         name=name,
         age=age,
-        frutas=frutas
+        frutas=frutas,
     )
 
 @app.route('/about')
 def about():
     return render_template(
-        'about.html'
+        'about.html',
+        notas=notas
     )
 
 app.run(debug=True)
