7ee62ab53065a78173c3c8ece20b44568575ae1a
Renato <renato_da_costa2000@yahoo.com.br>
2023-06-16 11:48:41 -0300

    feat: capture/work values at html

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
+frutas = ['Morango', 'Uva', 'Maçã', 'Laranja']
+
 app = Flask(__name__)
 
 @app.route('/')
 def principal():
+    name = 'renato'
+    age = 47
     return render_template(
-        'index.html'
+        'index.html',
+        name=name,
+        age=age,
+        frutas=frutas
     )
 
 @app.route('/about')
